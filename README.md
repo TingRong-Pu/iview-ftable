cb45261d7b67d318d3e375ccf757c722da9ecd73
zipai <zipai@2dfire.com>
2018-07-23 09:55:45 +0800

    first blood

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
-# iview-ftable
-A table component base on iView
+## 背景
+
+公司的项目需要用到iView，项目里的同事都觉得`Table`组件用起来不顺手，相对来说，element-ui 的`el-table`好理解。
+所以就有了这个小组件(无奈脸)
+
+## 注意事项
+
+`fTable` 只是更改了 `columns` 的写法，其他的 `prop` 和 `Table` 一致
+另外`Table` 的 `header`、`footer`、`loading` 3个 slot 没有实现
+
+## 用法
+
+```
+    // import
+    import fTable from "@/components/fTable/fTable";
+    import fTableColumn from "@/components/fTable/fTableColumn";
+
+    export default {
+        components: {
+            fTable,
+            fTableColumn
+        },
+        data() {
+            return {
+                data: [{
+                    name: "John Brown",
+                    age: 18,
+                    address: "New York No. 1 Lake Park",
+                    date: "2016-10-03"
+                }]
+            };
+        },
+    };
+```
+
+```
+    // template
+    <f-table :data="data">
+        <f-table-column type="expand" :width="60" align="center">
+            <template slot-scope="scope">
+                expand 的内容，可以是各种组件
+            </template>
+        </f-table-column>
+        <f-table-column type="selection" :width="60" align="center"></f-table-column>
+        <f-table-column title="名字" prop="name"></f-table-column>
+        <f-table-column title="操作">
+            <template slot-scope="scope">
+                <Button type="primary" @click="$Modal.info({
+                      title: '提示',
+                      content: `查看 ${scope.name} 的信息`
+                  })">查看</Button>
+                <Button type="warning" @click="$Modal.warning({
+                      title: '提示',
+                      content: `点击编辑 ${scope.name} 的信息`
+                  })">编辑</Button>
+                <Button type="error" @click="$Modal.error({
+                      title: '警告',
+                      content: `确定要删除用户： ${scope.name} ？`
+                  })">删除</Button>
+            </template>
+        </f-table-column>
+    </f-table>
+```
+## demo
+
+[here](https://codesandbox.io/s/km1ry8zrp7)
---
+++ src/components/Demo.vue
@@ -0,0 +1,127 @@
+<template>
+  <div class="demo">
+    <p>stripe:
+      <i-switch v-model="fTableOptions.stripe"></i-switch>
+    </p>
+    <p>border:
+      <i-switch v-model="fTableOptions.border"></i-switch>
+    </p>
+    <p>showHeader:
+      <i-switch v-model="fTableOptions.showHeader"></i-switch>
+    </p>
+    <p>loading:
+      <i-switch v-model="fTableOptions.loading"></i-switch>
+    </p>
+    <p>disabledHover:
+      <i-switch v-model="fTableOptions.disabledHover"></i-switch>
+    </p>
+    <p>highlightRow:
+      <i-switch v-model="fTableOptions.highlightRow"></i-switch>
+    </p>
+    <p>
+      <RadioGroup v-model="fTableOptions.size" type="button">
+        <Radio label="small"></Radio>
+        <Radio label="default"></Radio>
+        <Radio label="large"></Radio>
+      </RadioGroup>
+    </p>
+
+
+    <f-table :data="tabledata" v-bind="fTableOptions" @on-row-click="onRowClick">
+      <f-table-column type="expand" :width="60" align="center">
+        <template slot-scope="scope">
+             展开
+        </template>
+      </f-table-column>
+      <f-table-column type="selection" :width="60" align="center"></f-table-column>
+      <f-table-column title="Age" prop="age" :filters="[{label: 'Greater than 25',value: 1}]" :filterMethod="()=>(true) "></f-table-column>
+      <f-table-column title="Address" prop="address"></f-table-column>
+      <f-table-column title="Date" prop="date"></f-table-column>
+      <f-table-column title="Action">
+        <template slot-scope="scope">
+          <Button type="primary" @click="$Modal.info({
+                  title: '提示',
+                  content: `查看 ${scope.name} 的信息`
+              })">查看</Button>
+          <Button type="warning" @click="$Modal.warning({
+                  title: '提示',
+                  content: `点击编辑 ${scope.name} 的信息`
+              })">编辑</Button>
+          <Button type="error" @click="$Modal.error({
+                  title: '警告',
+                  content: `确定要删除用户： ${scope.name} ？`
+              })">删除</Button>
+        </template>
+      </f-table-column>
+    </f-table>
+
+
+
+  </div>
+</template>
+
+<script>
+import fTable from "@/components/fTable/fTable";
+import fTableColumn from "@/components/fTable/fTableColumn";
+export default {
+    components: {
+        fTable,
+        fTableColumn
+    },
+    data() {
+        return {
+            fTableOptions: {
+                stripe: false,
+                border: false,
+                showHeader: true,
+                loading: false,
+                disabledHover: false,
+                highlightRow: false,
+                size: "default",
+                noDataText: "暂无数据",
+                noFilteredDataText: "暂无筛选结果"
+            },
+            tabledata: [
+                {
+                    name: "John Brown",
+                    age: 18,
+                    address: "New York No. 1 Lake Park",
+                    date: "2016-10-03"
+                },
+                {
+                    name: "Jim Green",
+                    age: 24,
+                    address: "London No. 1 Lake Park",
+                    date: "2016-10-01"
+                },
+                {
+                    name: "Joe Black",
+                    age: 30,
+                    address: "Sydney No. 1 Lake Park",
+                    date: "2016-10-02"
+                },
+                {
+                    name: "Jon Snow",
+                    age: 26,
+                    address: "Ottawa No. 2 Lake Park",
+                    date: "2016-10-04"
+                }
+            ]
+        };
+    },
+    methods: {
+        onRowClick(args) {
+            console.log(args);
+        }
+    }
+};
+</script>
+
+<style>
+.demo {
+    padding: 20px;
+}
+.demo > p {
+    margin-bottom: 10px;
+}
+</style>
